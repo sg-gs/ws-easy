fb31662a71df3a8ffa73e1afe45a2f51a99b51e8
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-01-30 18:20:28 +0100

    feat(#5): main class for the library

--- src/socket.ts
+++ src/socket.ts
@@ -1,29 +1,51 @@
-import { Server } from 'http';
 import { Server as WebSocketServer } from 'ws';
 
-import { WebSocketEvent, WebSocketCloseEvent } from "./events";
+import { WebSocketNotExistsError, WebSocketNotFoundError } from './errors';
+import { WebSocketEvent, WebSocketCloseEvent, StandardEvents, WebSocketErrorEvent, WebSocketCustomEvent } from "./events";
 
-export default class WsEasy {
-
-    private wsServer: WebSocketServer; 
+export default class WsEasyInitializer {
+    constructor (host: string, port: number) {
+        const server = new WebSocketServer({ host, port });
+        new WsEasy(server);
+    }
+}
+class WsEasy {
+    private wsServer: WebSocketServer;
     private ws!: WebSocket;
     private events: Map<String, WebSocketEvent>;
 
-    constructor(server: Server) {
-        this.wsServer = new WebSocketServer({ server });
+    constructor(server: WebSocketServer) {
+        this.wsServer = server;
 
-        this.events = new Map<String, WebSocketEvent>();
-        this.events.set('close', new WebSocketCloseEvent());
-        // TODO: On connection, on message, on error ...
+        this.events = new Map<StandardEvents | string, WebSocketEvent>();
+        this.events.set(StandardEvents.close, new WebSocketCloseEvent());
+        this.events.set(StandardEvents.error, new WebSocketErrorEvent());
 
         this.wsServer.on('connection', (socket: WebSocket) => {
             this.ws = socket;
         });
     }
 
-    public on (event:string) : void {
-       if(this.ws) {
-        // TODO: Use events map
-       } 
+    public on(event: StandardEvents | string, cb: Function): void {
+
+        if (!(this.ws instanceof WebSocket)) throw new WebSocketNotFoundError();
+
+        if (!this.ws) throw new WebSocketNotExistsError();
+
+        const exists = this.events.has(event);
+        const notNull = this.events.get(event);
+
+        if(exists && notNull) {
+            this.events.get(event)?.using(this.ws).setCallback(cb); 
+        } else {
+            const add = this._add;
+            const customEvent = new WebSocketCustomEvent(event);
+            customEvent.using(this.ws).setCallback(cb);
+            add(customEvent);
+        }         
+    }
+
+    private _add (wsEvent: WebSocketEvent | WebSocketCustomEvent): void {
+        this.events.set(wsEvent.name, wsEvent);
     }
 }
